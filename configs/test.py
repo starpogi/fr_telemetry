be83a9b5e89788ae4935228679934cb635019cdc
Javier Onglao <javier@easypost.com>
2018-09-29 04:31:10 -0700

    Um, try this

--- configs/test.py
+++ configs/test.py
@@ -1,6 +1,6 @@
 
 
 class Test:
-    SQLALCHEMY_DATABASE_URI = "mysql://ubuntu:@localhost/circle_test"
+    SQLALCHEMY_DATABASE_URI = "mysql://ubuntu:@localhost:3306/circle_test"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     TESTING = True
